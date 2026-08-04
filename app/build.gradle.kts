afdfbed9f8d5699e6f017d22caed50fd4b267ea4
CyferBoy <70091085+CyferBoy@users.noreply.github.com>
2026-08-04 12:14:04 +0530

    Update app version to v1.6 (code 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.neochildclinic"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 6
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
