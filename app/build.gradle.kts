977f0a8ac392e29238a639a65a66b704f9252e8a
CyferBoy <70091085+CyferBoy@users.noreply.github.com>
2026-08-16 07:24:24 +0530

    Consolidate app versioning into Version Catalog (libs.versions.toml)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.neochildclinic"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.5.0"
+        versionCode = libs.versions.appVersionCode.get().toInt()
+        versionName = libs.versions.appVersionName.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
